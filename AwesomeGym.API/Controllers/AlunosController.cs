a63a2b51ee4d2e803429d0231ef7e3facb977704
Luis Felipe <luisfelipekai@gmail.com>
2020-10-21 16:22:32 -0300

    Criando estrutura básica com pacote e ConnectionString.

---
+++ AwesomeGym.API/Controllers/AlunosController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace AwesomeGym.API.Controllers
+{
+    [ApiController]
+    [Route("api/alunos")]
+    public class AlunosController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
